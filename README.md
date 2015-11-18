adf8cc4bb29549e5ba7f9a936330bf6c2d393b7b
Atul <atul@chef.io>
2015-11-18 13:51:19 +0530

    simple-web cookbook

---
+++ README.md
@@ -0,0 +1,7 @@
+# simple-web
+
+TODO: Enter the cookbook description here.
+
+holla
+
+And HERE!!!
---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+default['simple-web']['company-name'] = 'Chef Software Inc. '
+default['simple-web']['project-name'] = 'Chef Delivery Demo'
