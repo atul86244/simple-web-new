58850e837c52c45d7ee7452b5a0343536f14ced8
Atul <atul@chef.io>
2015-11-20 10:47:59 +0530

    demo test

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ maintainer_email 'admin@chef.io'
 license          'all_rights'
 description      'Installs/Configures simple-web'
 long_description 'Installs/Configures simple-web'
-version          '0.4.39'
+version          '0.4.40'
 
 depends 'apt'
 depends 'httpd'
